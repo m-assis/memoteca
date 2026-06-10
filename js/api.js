35191e05932bc6e777ed9582bc4c2df39f93d78e
Mike Assis <mikheas.assis@gmail.com>
2026-06-10 11:57:56 -0300

    feat: adicionando requisições de GET E POST

--- js/api.js
+++ js/api.js
@@ -4,11 +4,26 @@ const api = {
       const response = await fetch('http://localhost:3000/pensamentos')
       return await response.json()
     }
-    catch {
+    catch (error) {
       alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao salvar pensamento')
       throw error
     }
   }
 }
 
-export default api
+export default api;
